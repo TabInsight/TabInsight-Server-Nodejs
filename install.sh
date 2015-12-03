3d7c76eee21c0b1c0f73cfe31236c904ec121835
Bijil <bijilap@gmail.com>
2015-12-02 22:27:09 -0800

    adding install

---
+++ install.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+sudo apt-get update
+
+#setup nodejs
+sudo apt-get install nodejs
+sudo apt-get install npm
+sudo npm install -g express-generator
+sudo apt-get install nodejs-legacy
+sudo npm install -g forever
+
+#setup app
+mkdir /opt/tabinsight-server-v2
+cp -r ./app /opt/tabinsight-server-v2
+sudo chmod -R 755 /opt/tabinsight-server-v2
+cd /opt/tabinsight-server-v2/app
+npm install
+
+#setting up forever to keep the app running
+forever start /opt/tabinsight-server-v2/app/app.js
+#forever stop /opt/tabinsight-server-v2/app/app.js
+sudo touch /etc/init/tabinsight-server-v2.conf
+sudo echo "start on startup" >>/etc/init/tabinsight-server-v2.conf
+sudo echo "/opt/tabinsight-server-v2/app/app.js" >>/etc/init/tabinsight-server-v2.conf
