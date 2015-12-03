4605c4e22609d0ff42bdf34d29b1921a07ef3681
hemantku <vgh4uster@gmail.com>
2015-12-02 22:31:46 -0800

    Log Logic

--- app/routes/publish.js
+++ app/routes/publish.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var csv_writter = require("fast-csv");
-
+var fs  = require('fs');
 var LOG_DIR = '/var/tabinsight/data/'
 
 function createLogFile(){
@@ -18,10 +18,22 @@ router.post('/log', function(req,res){
     sample request:
     {"device": "xyz", "access_time": "123", "app_name": "abc", "usetime": "1"}
   */
-  var user_id = req.param('id');
-  var token = req.param('token');
-  var geo = req.param('geo');
-  res.send(user_id + ' ' + token + ' ' + geo);
+  if (!req.body) return res.sendStatus(400)
+  var post_data = req.body;
+  var deviceid = req.body.device;
+  var accesstime = req.body.access_time;
+  var appname = req.body.app_name;
+  var use_time = req.body.usetime;
+  var filename = LOG_DIR + deviceid + ".csv"
+  var csvStream = csv_writter.createWriteStream({headers: false}),
+  writableStream = fs.createWriteStream(filename);
+  
+  writableStream.on("finish", function(){
+    console.log("CSV File written! LogStash Crunching!");
+  });
+  csvStream.pipe(writableStream);
+  csvStream.write({device: deviceid, access_time: accesstime, app_name: appname.split(' ').join('_'), usetime:use_time});
+  csvStream.end()
 });
 
 router.post('/logs', function(req,res){
